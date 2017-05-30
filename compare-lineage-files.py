72a0e15144c88751078c546dd537823f5194b4fb
C. Titus Brown <titus@idyll.org>
2017-05-30 16:47:48 -0700

    add count filtering on a

--- compare-lineage-files.py
+++ compare-lineage-files.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 from __future__ import print_function
+import sys
 import argparse
+from collections import Counter
 
 want_taxonomy = ['superkingdom', 'phylum', 'order', 'class', 'family', 'genus', 'species']
 
@@ -9,6 +11,7 @@ def main():
     p.add_argument('file1')
     p.add_argument('file2')
     p.add_argument('column')
+    p.add_argument('-m', '--min-in-file1', type=int, default=0)
     args = p.parse_args()
 
     try:
@@ -16,8 +19,9 @@ def main():
     except ValueError:
         column_idx = want_taxonomy.index(args.column)
 
-    a = set()
+    a_cnt = Counter()
 
+    print('loading', args.file1, file=sys.stderr)
     with open(args.file1) as fp:
         next(fp)
         
@@ -26,9 +30,17 @@ def main():
             if len(col):
                 col = col.split(';')
                 col = col[column_idx]
-                a.add(col)
+                a_cnt[col] += 1
+
+    a = set()
+    for item, item_count in a_cnt.most_common():
+        if item_count >= args.min_in_file1:
+            a.add(item)
+        else:
+            break
 
     b = set()
+    print('loading', args.file2, file=sys.stderr)
     with open(args.file2) as fp:
         next(fp)
         
